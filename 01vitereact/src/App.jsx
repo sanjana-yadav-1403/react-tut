b895615f0a675b37afb4b6615fd1fa36a1251673
Sanjana Yadav <sanjanayadav1403@gmail.com>
2024-09-09 22:04:50 +0530

    commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Example from './example.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+    <Example />
+    <h1>React Series | Sanjana Yadav</h1>
+    </>
+    
+  )
+}
+
+export default App;
---
+++ 01vitereact/src/example.jsx
@@ -0,0 +1,9 @@
+function Example(){
+    return(
+        <>
+            <h1>This is sanjana.</h1>
+        </>
+    )
+}
+
+export default Example
